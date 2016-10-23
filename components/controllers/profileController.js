6f783f72ff2f570816f05d28f613b2562544c029
Fernando Navarijo <enavarijo@xik.gt>
2016-10-22 21:21:12 -0700

    Edit user interface

--- components/controllers/profileController.js
+++ components/controllers/profileController.js
@@ -5,12 +5,30 @@ require('../services/perfilService');
         .controller('ProfileController', profileFunc);
     
     function profileFunc(GetUserService){
-        this.name= 'meee';
+        this.user= {};
+        this.userBk = {};
+        this.editUser = false;
 
-        GetUserService.get({userId: '24', tipo: 'Empresa'})
+        GetUserService.get({userId: '38', tipo: 'Persona'})
             .then((result)=> {
-                console.log('RESULT', result.data)
+                this.user = result.data;
+                this.userBk = Object.assign({}, this.user);
             }, (err)=>console.log('ERR', err));
+
+        this.enableEdit = function () {
+            if(this.editUser) this.user = Object.assign({}, this.userBk);
+            this.editUser = !this.editUser;
+            console.log('EDITING', this.editUser);
+        }
+
+        this.disableEdit = function () {
+            this.editUser = false;
+            this.user = Object.assign({}, this.userBk);
+        }
+
+        this.updateUser = function(){
+            console.log('UPDATING', this.user, this.userBk);
+        }
     }
 
 })();
